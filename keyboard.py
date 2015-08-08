daa7931ca793e5341c49e31b4754f13f26f99dfe
san2488 <sujay.narsale@gmail.com>
2015-08-07 21:30:11 -0700

    Onscreen tic tac toe works

---
+++ keyboard.py
@@ -0,0 +1,9 @@
+
+def getLetter():
+    return raw_input().upper()
+
+def getMove():
+    return raw_input()
+
+def getYesOrNo():
+    return raw_input().lower().startswith('y')
---
+++ screen.py
@@ -0,0 +1,20 @@
+
+
+def drawBoard(board):
+    # This function prints out the board that it was passed.
+
+    # "board" is a list of 10 strings representing the board (ignore index 0)
+    print('   |   |')
+    print(' ' + board[7] + ' | ' + board[8] + ' | ' + board[9])
+    print('   |   |')
+    print('-----------')
+    print('   |   |')
+    print(' ' + board[4] + ' | ' + board[5] + ' | ' + board[6])
+    print('   |   |')
+    print('-----------')
+    print('   |   |')
+    print(' ' + board[1] + ' | ' + board[2] + ' | ' + board[3])
+    print('   |   |')
+
+def prompt(text):
+    print(text)
